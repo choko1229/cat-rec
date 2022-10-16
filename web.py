822369feb9e9a1e36dc3680b94a6f8a637c5c947
choko1229 <67551804+choko1229@users.noreply.github.com>
2022-10-16 14:58:26 +0900

    Update web.py

--- web.py
+++ web.py
@@ -2,15 +2,23 @@ import os
 import shutil
 import datetime
 import time
+
+#無限ループ
 while True:
- os.chdir('画像出力フォルダパス')
+#出力先フォルダを選択
+ os.chdir('/media/choko1229/server/Dev/cat-rec/save/')
+#output.jpgを探す
  if "output.jpg" in os.listdir():
   cmo = 99
   csa = 100
   time.sleep(0.01)
-  shutil.move("出力画像パス", "Webサーバー画像フォルダ")
-  os.chdir('Webサーバー画像フォルダ')
+#output.jpgを0.jpgとしてWeb公開ページに移動
+  shutil.move("/media/choko1229/server/Dev/cat-rec/save/output.jpg", "/var/www/html/dev/cat-rec/img/0.jpg")
+#Web公開フォルダを選択
+  os.chdir('/var/www/html/dev/cat-rec/img/')
+#処理が完了するまでループ
   while csa > 0:
+#n.jpgをn+1.jpgに変更
    os.rename(str(cmo) + ".jpg",str(csa) + ".jpg")
    cmo -= 1
    csa -= 1
